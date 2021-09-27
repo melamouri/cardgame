5c3a1274d7756193671e790fea7c6b98d75faa85
Mohamed EL AMOURI <melamouri@infopro-digital.com>
2021-09-27 23:46:34 +0200

    ajout TU

--- src/test/java/com/atexo/cardgame/controller/CardControllerTest.java
+++ src/test/java/com/atexo/cardgame/controller/CardControllerTest.java
@@ -1,6 +1,8 @@
 package com.atexo.cardgame.controller;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,7 +21,6 @@ import com.atexo.cardgame.Dto.HandDto;
 import com.atexo.cardgame.Enum.CardColorEnum;
 import com.atexo.cardgame.Enum.CardValueEnum;
 import com.atexo.cardgame.service.CardService;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CardController.class)
@@ -38,10 +39,39 @@ public class CardControllerTest {
 
     /**
      * verifie que le retour est bien un json.
+     * 
      * @throws Exception
      */
     @Test
     public void getHand_thenReturnJsonArray() throws Exception {
+        HandDto hand = initHand();
+        Mockito.when(cardService.getHand()).thenReturn(hand);
+
+        mvc.perform(get("/api/hand").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.cards[0].color",
+                        is(CardColorEnum.CARREAUX.toString())))
+                .andExpect(jsonPath("$.cards[0].value",
+                        is(CardValueEnum.SIX.toString())));
+    }
+
+    @Test
+    public void getSortedHand_thenReturnJsonArray() throws Exception {
+        String handPram = new String(getClass().getClassLoader()
+                .getResourceAsStream("hand.json").readAllBytes());
+        HandDto sortedHand = initSortedHand();
+        Mockito.when(cardService.getSortedHand(Mockito.any(), Mockito.any(),
+                Mockito.any())).thenReturn(sortedHand);
+        mvc.perform(post("/api/sortedHand").content(handPram)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.cards[0].color",
+                        is(CardColorEnum.PIQUE.toString())))
+                .andExpect(jsonPath("$.cards[0].value",
+                        is(CardValueEnum.DEUX.toString())));
+    }
+
+    private HandDto initHand() {
         HandDto hand = new HandDto();
         hand.getCards()
                 .add(new CardDto(CardColorEnum.CARREAUX, CardValueEnum.SIX));
@@ -63,19 +93,32 @@ public class CardControllerTest {
                 .add(new CardDto(CardColorEnum.COEUR, CardValueEnum.ROI));
         hand.getCards()
                 .add(new CardDto(CardColorEnum.PIQUE, CardValueEnum.DEUX));
-
-        Mockito.when(cardService.getHand()).thenReturn(hand);
-
-        mvc.perform(get("/api/hand").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.cards[0].color",
-                        is(CardColorEnum.CARREAUX.toString())))
-                .andExpect(jsonPath("$.cards[0].value",
-                        is(CardValueEnum.SIX.toString())));
+        return hand;
     }
 
-//    @Test
-//    public void getSortedHand_thenReturnJsonArray() {
-//
-//    }
+    private HandDto initSortedHand() {
+        HandDto hand = new HandDto();
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.PIQUE, CardValueEnum.DEUX));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.PIQUE, CardValueEnum.DAME));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.CARREAUX, CardValueEnum.DIX));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.CARREAUX, CardValueEnum.SIX));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.TREFLE, CardValueEnum.AS));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.TREFLE, CardValueEnum.DAME));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.COEUR, CardValueEnum.DIX));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.COEUR, CardValueEnum.DAME));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.COEUR, CardValueEnum.ROI));
+        hand.getCards()
+                .add(new CardDto(CardColorEnum.COEUR, CardValueEnum.VALET));
+
+        return hand;
+    }
 }
